81d40611026a9c8e2be3b41db7e05adc5db99425
ljw6731 <henry49466336@gmail.com>
2021-04-10 13:16:57 +0900

    aa

---
+++ 1-4turtle.py
@@ -0,0 +1,41 @@
+from turtle import forward as 앞으로
+from turtle import backward as 뒤로
+from turtle import mainloop as 그대로두세요
+from turtle import left as 왼쪽
+from turtle import right as 오른쪽
+from turtle import penup as 그만그려
+from turtle import pendown as 그려
+from turtle import pensize as 굵기
+from turtle import speed as 속도
+from turtle import pencolor as 색
+from turtle import clear as 지우기
+from turtle import goto as 이동
+from turtle import circle as 원
+
+import random
+import turtle
+import winsound
+turtle.shape("turtle")
+
+# def 랜덤색():
+#     색종류 = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+#     색(색종류[random.randint(0,6)])
+
+
+# 색의 종류 red, orange, yellow, green, blue, indigo, violet
+속도(10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+
+
+# for i in range(10):
+#     사각형()
+#     왼쪽(10)
+while True:
+    # 랜덤색()
+    굵기(10)
+    앞으로(10)
+    왼쪽(10)
+    원(100)
+
+
+
+그대로두세요()
