e77e6ca466ea6717d9e3700d9de733d540ac0834
ljw6731 <henry49466336@gmail.com>
2021-04-24 13:15:09 +0900

    aaa

---
+++ 2-1.py
@@ -0,0 +1,47 @@
+# a = float(123)
+# b = float(123.12)
+# print(type(a), type(b))
+# print(a, b)
+
+# a = 123.
+# b = 123.12
+# print(type(a), type(b))
+
+# a = '여기는\n와이즈만\n입니다.'
+# b ='''여기는
+# 와이즈만
+# 입니다
+# '''
+# c = '여기는\t 와이즈만\t 입니다.\t 여기는\t 집\t 입니다.'
+# print(type(a))
+# print(a)
+# print(b)
+# print(c)
+
+score=str(100)
+sen='멋지네요!'
+sen1='다음에 다시 도전하세요'
+score1=(0)
+# print('당신의 점수는 %s점 입니다.%s'%(score,sen))
+# '''아니면'''
+# score2=str(0)
+# sen2='다음에 다시 도전하세요!'
+# print('당신의 점수는 %s점 입니다.%s'%(score2,sen2))
+# print('당신의 점수는 {}점 입니다.{}'.format(score, sen))
+# print('당신의 점수는 {}점 입니다.{}'.format(score1, sen1))
+# print(f'당신의 점수는 {score}점 입니다. {sen}')
+# print(f'당신의 점수는 {score1}점 입니다. {sen1}')
+# print('당신의 점수는 {0}점 입니다.{1}'.format(score, sen))
+# string='여기는 와이즈만 입니다'
+# print(string[0:5])
+# print(string[2:5:1])
+# print(string[2:5:2])
+# print(string[::-1])
+string='       this is my python project.ppp'
+print(len(string))
+print(string.find('p'))
+print(string.count('p'))
+print(string)
+print(string.strip())
+string_split= string.split()
+print(string_split)
